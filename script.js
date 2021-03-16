0150f189da3913c3516cd6a73945655698b6bf0a
Endy de Almeida <endy@outlook.com>
2021-03-16 17:48:03 -0300

    Add a loading gif

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-const DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
+const dog_url = "https://dog.ceo/api/breeds/image/random";
 const doggo = document.querySelector(".doggo");
-let hasdoggo = false;
+const img = document.createElement("img");
 
 function generateDoggo() {
-  if (hasdoggo === false) {
-  const promise = fetch(DOG_URL);
-  promise
-    .then(function(response) {
-      const processingPromise = response.json();
-      return processingPromise;
-    })
-    .then(function(processedResponse) {
-      const img = document.createElement("img");
-      img.src = processedResponse.message;
-      img.alt = "Cute doggo";
-      doggo.appendChild(img);
-    });
-  hasdoggo = true;
-  } else {
-    const img = document.querySelector("img");
-    img.remove();
-    hasdoggo = false;
-    generateDoggo();
+    img.src = "./loading.gif";
+    img.alt = "Loading";
+    doggo.appendChild(img);
+     const promise = fetch(dog_url);
+    promise
+      .then(function(response) {
+        const processingPromise = response.json();
+        return processingPromise;
+        })
+      .then(function(processedResponse) {
+        img.src = processedResponse.message;
+        img.alt = "Cute doggo";
+        });
   }
-}
 
 document.querySelector(".generate-doggo").addEventListener("click", generateDoggo);
