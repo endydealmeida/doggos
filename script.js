31883e579e44ecd851aa4a323ab46e46809a2f8c
Endy de Almeida <endy@outlook.com>
2021-03-17 17:06:14 -0300

    Fetch the breeds list

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 const dog_url = "https://dog.ceo/api/breeds/image/random";
-const doggo = document.querySelector(".doggo");
+const breeds_url = "https://dog.ceo/api/breeds/list/all";
 const img = document.createElement("img");
 
-function generateDoggo() {
-    img.src = "./loading.gif";
-    img.alt = "Loading";
-    doggo.appendChild(img);
-     const promise = fetch(dog_url);
-    promise
-      .then(function(response) {
-        const processingPromise = response.json();
-        return processingPromise;
+const promise = fetch(breeds_url);
+promise
+  .then(function (response) {
+    const processingPromise = response.json();
+    return processingPromise;
+  })
+  .then(function (processedResponse) {
+    const breeds_list = processedResponse.message;
+    const div_select = document.querySelector(".div-select");
+    const breeds_select = document.createElement("select");
+    div_select.appendChild(breeds_select);
+    const option = document.createElement("option");
+    option.textContent = "random";
+    option.value = "random";
+    breeds_select.appendChild(option);
+
+    Object.keys(breeds_list).forEach(function (key) {
+      if (breeds_list[key].length === 0) {
+        const option = document.createElement("option");
+        option.textContent = key;
+        option.value = key;
+        breeds_select.appendChild(option);
+      } else {
+        breeds_list[key].forEach(subBreed => {
+          const option = document.createElement("option");
+          option.textContent = key + " " + subBreed;
+          option.value = key + " " + subBreed;
+          breeds_select.appendChild(option);
         })
-      .then(function(processedResponse) {
-        img.src = processedResponse.message;
-        img.alt = "Cute doggo";
-        });
-  }
+      }
+    });
+  })
+
+function generateDoggo() {
+  const doggo = document.querySelector(".div-doggo");
+  img.src = "./loading.gif";
+  img.alt = "Loading";
+  doggo.appendChild(img);
+  const promise = fetch(dog_url);
+  promise
+    .then(function (response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function (processedResponse) {
+      img.src = processedResponse.message;
+      img.alt = "Cute doggo";
+    });
+}
 
 document.querySelector(".generate-doggo").addEventListener("click", generateDoggo);
